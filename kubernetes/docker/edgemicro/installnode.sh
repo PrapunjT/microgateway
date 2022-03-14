23b1a2dac244361b779b186d52b4ba85348c59be
PrapunjT <tprapunj@gmail.com>
2022-03-14 09:59:20 +0000

    docker changes

--- kubernetes/docker/edgemicro/installnode.sh
+++ kubernetes/docker/edgemicro/installnode.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 set echo off
-npm install --only=production --no-optional PrapunjT/microgateway#docker-changes
+
+# For development purpose
+apt-get update && apt-get install -y git python build-essential
+
+cd /opt/apigee
+git clone -b docker-changes https://github.com/PrapunjT/microgateway.git
+cd microgateway
+npm install
+npm link
+
+# npm install --only=production --no-optional -g edgemicro
